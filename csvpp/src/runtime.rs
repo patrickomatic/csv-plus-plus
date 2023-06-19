21817be80ee057afde88d99fc110e97dd3dd78c6
Patrick <patrick@patrickomatic.com>
2023-06-19 19:54:48 -0400

    rust rewrite: parse cell ASTs

--- csvpp/src/runtime.rs
+++ csvpp/src/runtime.rs
@@ -6,6 +6,7 @@ use std::fmt;
 use crate::{CliArgs, Init, Modifier, Options, OutputTarget, Result, SourceCode, Template};
 use crate::compiler::token_library::TokenLibrary;
 
+#[derive(Debug)]
 pub struct Runtime {
     pub default_modifier: Modifier,
     pub options: Options,
@@ -17,7 +18,11 @@ pub struct Runtime {
 
 impl Runtime {
     pub fn from_cli_args() -> Result<Self> {
-        let init = Init::from_cli_args(CliArgs::parse())?;
+        Self::new(CliArgs::parse())
+    }
+
+    pub fn new(cli_args: CliArgs) -> Result<Self> {
+        let init = Init::from_cli_args(cli_args)?;
         // TODO this needs to merge in variables from both the CLI and the runtime variables
 
         Ok(Self {
@@ -50,19 +55,24 @@ impl fmt::Display for Runtime {
 
 #[cfg(test)]
 mod tests {
+    use std::path::PathBuf;
+
     use super::*;
 
-    fn from_cli_args() {
-        /* TODO
-        Runtime::new(Options
+    #[test]
+    fn new() {
+        let mut cli_args = CliArgs::default();
+        cli_args.input_filename = PathBuf::from("foo.csvpp");
+        cli_args.google_sheet_id = Some("abc123".to_string());
 
-        }
-        */
+        let runtime = Runtime::new(cli_args);
+        assert!(runtime.is_ok())
     }
 
     #[test]
     fn display() {
         // let output = 
+        // TODO
     }
 }
 
