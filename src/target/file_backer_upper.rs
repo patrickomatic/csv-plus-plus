8a4675ce22db597a32ddba9c727460e127044070
Patrick <patrick@patrickomatic.com>
2023-09-19 06:29:09 -0400

    make backup_file crate-private

--- src/target/file_backer_upper.rs
+++ src/target/file_backer_upper.rs
@@ -6,13 +6,13 @@ use std::path;
 use crate::{Error, Result};
 
 const BACKUP_FORMATS: &[&str] = &[
-    // filename.csv -> filename2023-04-25.csv
+    // filename.csv -> filename-2023-04-25.csv
     "%Y_%m_%d",
-    // filename.csv -> filename2023-04-25-1_00AM.csv
+    // filename.csv -> filename-2023-04-25-1_00AM.csv
     "%Y_%m_%d-%I_%M%p",
-    // filename.csv -> filename2023-04-25-1_00_00AM.csv
+    // filename.csv -> filename-2023-04-25-1_00_00AM.csv
     "%Y_%m_%d-%I_%M_%S%p",
-    // filename.csv -> filename2023-04-25-1_00_00_0000AM.csv
+    // filename.csv -> filename-2023-04-25-1_00_00_0000AM.csv
     "%Y_%m_%d-%I_%M_%S_%f%p",
 ];
 
@@ -21,7 +21,7 @@ const BACKUP_FORMATS: &[&str] = &[
 // NOTE:
 // this operation is not technically atomic - to do so we'd need to create a tempfile, write to it
 // then move it in place.  (but for this use case I don't think it matters)
-pub fn backup_file(filename: &path::PathBuf) -> Result<path::PathBuf> {
+pub(crate) fn backup_file(filename: &path::PathBuf) -> Result<path::PathBuf> {
     let now = Local::now();
 
     // TODO use a TargetError instead
@@ -30,25 +30,22 @@ pub fn backup_file(filename: &path::PathBuf) -> Result<path::PathBuf> {
     ))?;
 
     let file_stem = filename.file_stem().ok_or(Error::InitError(format!(
-        "Unable to get base file for: {}",
-        filename_str
+        "Unable to get base file for: {filename_str}",
     )))?;
 
     let file_parent = filename.parent().ok_or(Error::InitError(format!(
-        "Unable to get parent base file for: {}",
-        filename_str
+        "Unable to get parent base file for: {filename_str}",
     )))?;
 
     let file_extension = filename.extension().ok_or(Error::InitError(format!(
-        "Unable to get extension for: {}",
-        filename_str
+        "Unable to get extension for: {filename_str}",
     )))?;
 
     for time_format in BACKUP_FORMATS.iter() {
         let timestamp = now.format(time_format);
 
         let mut new_file = file_parent.to_path_buf();
-        new_file.push(format!("{}{}", file_stem.to_str().unwrap(), timestamp));
+        new_file.push(format!("{}-{timestamp}", file_stem.to_str().unwrap()));
         new_file.set_extension(file_extension);
 
         if new_file.exists() {
@@ -57,8 +54,7 @@ pub fn backup_file(filename: &path::PathBuf) -> Result<path::PathBuf> {
 
         if let Err(e) = fs::copy(filename, &new_file) {
             return Err(Error::InitError(format!(
-                "Error making backup of {}: {}",
-                filename_str, e
+                "Error making backup of {filename_str}: {e}",
             )));
         }
 
@@ -66,8 +62,7 @@ pub fn backup_file(filename: &path::PathBuf) -> Result<path::PathBuf> {
     }
 
     Err(Error::InitError(format!(
-        "Unable to make backup of output file: {}",
-        filename_str
+        "Unable to make backup of output file: {filename_str}",
     )))
 }
 
