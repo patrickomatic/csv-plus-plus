6785ca3a9c8c78ff19c666e2622dabdd4ddd7935
Patrick <patrick@patrickomatic.com>
2024-02-11 23:03:04 -0500

    debug

--- tests/module_loading.rs
+++ tests/module_loading.rs
@@ -1,5 +1,6 @@
 mod common;
 use common::*;
+use std::process::Command;
 
 #[test]
 fn module_loading_and_variable_shadowing() {
@@ -18,10 +19,13 @@ fn module_loading_invalidate_cache() {
     // we keep around each result so it doesn't get cleaned up and file removed
     let _s1 = compile_str("module_loading_invalidate_cache", "foo := 1\n---\n=foo");
     dbg!("1");
+    dbg!(Command::new("ls").args(["-l"]).output().unwrap());
     let _s2 = compile_str("module_loading_invalidate_cache", "foo := 2\n---\n=foo");
     dbg!("2");
+    dbg!(Command::new("ls").args(["-l"]).output().unwrap());
     let s3 = compile_str("module_loading_invalidate_cache", "foo := 3\n---\n=foo");
     dbg!("3");
+    dbg!(Command::new("ls").args(["-l"]).output().unwrap());
 
     assert_eq!(s3.unwrap().read_output(), "=3\n");
 }
